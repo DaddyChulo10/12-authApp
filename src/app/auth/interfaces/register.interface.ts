793d24659c6db9517b3f032b85ad82b82f35781a
DaddyChulo10 <edgtarkalim@gmail.com>
2024-03-12 12:51:37 -0600

    Se creo el modulo de crear usuario

---
+++ src/app/auth/interfaces/register.interface.ts
@@ -0,0 +1,13 @@
+export interface RegisterUserspost {
+  user:  User;
+  token: string;
+}
+
+export interface User {
+  email:    string;
+  name:     string;
+  isActive: boolean;
+  roles:    string[];
+  _id:      string;
+  __v:      number;
+}
